62f16cf5f1813b54ba5fefbe844c3e9eec4689df
jcrobinson004 <jcrobinson004@gmail.com>
2024-05-26 22:36:34 -0400

    update to routes

--- service/routes.py
+++ service/routes.py
@@ -72,7 +72,7 @@ def get_account_list_():
 
     accounts = Account.all()
     account_list = [account.serialize() for account in accounts]
-    
+
     app.logger.info("Returning [%s] accounts", len(account_list))
     return jsonify(account_list), status.HTTP_200_OK
 
@@ -97,6 +97,7 @@ def get_accounts(account_id):
 ######################################################################
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["PUT"])
 def update_accounts(account_id):
     """
@@ -106,18 +107,17 @@ def update_accounts(account_id):
     app.logger.info("Request to update an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-         abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
-        
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+
     account.deserialize(request.get_json())
     account.update()
 
     return account.serialize(), status.HTTP_200_OK
 
-
-
 ######################################################################
 # DELETE AN ACCOUNT
 ######################################################################
+
 @app.route("/accounts/<int:account_id>", methods=["DELETE"])
 def delete_accounts(account_id):
     """
